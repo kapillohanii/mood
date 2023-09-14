aeeb869af2a9d66d47531f4870041867d4338d34
kapillohanii <kapillohani023@gmail.com>
2023-09-14 23:53:58 +0530

    sign up/in ui

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,5 +2,5 @@ import { SignIn } from "@clerk/nextjs";
 
 
 export default function SignInPage(){
-    return <SignIn />;
+    return <div className="w-screen h-screen bg-black flex justify-center items-center text-white"><SignIn  /></div>;
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,5 +2,5 @@ import { SignUp } from "@clerk/nextjs";
 
 
 export default function SignUpPage(){
-    return <SignUp />;
+    return <div className="w-screen h-screen bg-black flex justify-center items-center text-white"><SignUp /></div>
 }
